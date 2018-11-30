e368d8c5b250bcf178d93b04d5cb58b084993ccf
Pedro Lizarraga <plizarraga@fender.com>
2018-11-29 19:20:58 -0800

    Setup all the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ClientsComponent } from './clients/clients.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ClientEditComponent } from './client-edit/client-edit.component';
+import { ClientAddComponent } from './client-add/client-add.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SettingsComponent } from './settings/settings.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ClientsComponent,
+    ClientEditComponent,
+    ClientAddComponent,
+    ClientDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
